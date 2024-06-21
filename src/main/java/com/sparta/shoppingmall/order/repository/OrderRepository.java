bcd3b575b30c251929cd6dc544d72caad15e7be0
LSH <k01094060462@gmail.com>
2024-06-21 17:21:05 +0900

    Fix(feature/order): CamelCase

--- src/main/java/com/sparta/shoppingmall/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/shoppingmall/order/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package com.sparta.shoppingmall.order.repository;
 
+import com.sparta.shoppingmall.order.dto.OrderResponseDto;
 import com.sparta.shoppingmall.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserid(Long userId);
+    List<OrderResponseDto> findByUserId(Long userId);
 }
