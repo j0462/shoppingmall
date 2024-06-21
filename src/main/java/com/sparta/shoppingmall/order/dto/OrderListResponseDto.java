b8032716454c95fbb1957bc6f8e81b2081d201f9
LSH <k01094060462@gmail.com>
2024-06-21 17:20:16 +0900

    Fix(feature/order): 빌더제거, Order>OrderResponseDto로 형 변환

--- src/main/java/com/sparta/shoppingmall/order/dto/OrderListResponseDto.java
+++ src/main/java/com/sparta/shoppingmall/order/dto/OrderListResponseDto.java
@@ -1,17 +1,14 @@
 package com.sparta.shoppingmall.order.dto;
 
-import com.sparta.shoppingmall.order.entity.Order;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class OrderListResponseDto {
-    private final List<Order> orders;
+    private final List<OrderResponseDto> orders;
 
-    @Builder
-    public OrderListResponseDto(List<Order> orders) {
+    public OrderListResponseDto(List<OrderResponseDto> orders) {
         this.orders = orders;
     }
 }
