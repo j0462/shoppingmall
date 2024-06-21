d230c1386cb3d3ecc7378ca9288fb056d3d687d8
LSH <k01094060462@gmail.com>
2024-06-21 17:19:43 +0900

    Fix(feature/order): 빌더제거, Notblank적용

--- src/main/java/com/sparta/shoppingmall/order/dto/OrderRequestDto.java
+++ src/main/java/com/sparta/shoppingmall/order/dto/OrderRequestDto.java
@@ -1,15 +1,16 @@
 package com.sparta.shoppingmall.order.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderRequestDto {
+    @NotBlank(message = "배달 주소는 공백이 될 수 없습니다.")
     private String address;
+    @NotBlank(message = "지불액은 공백이 될 수 없습니다.")
     private int totalPrice;
 }
