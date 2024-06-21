34c6113fca7e4e4b8e65d8701f1efa921ada4a73
LSH <k01094060462@gmail.com>
2024-06-21 17:21:31 +0900

    Fix(feature/order): 빌더제거, User를 받도록 변경

--- src/main/java/com/sparta/shoppingmall/order/dto/OrderResponseDto.java
+++ src/main/java/com/sparta/shoppingmall/order/dto/OrderResponseDto.java
@@ -2,21 +2,19 @@ package com.sparta.shoppingmall.order.dto;
 
 import com.sparta.shoppingmall.order.entity.Order;
 import com.sparta.shoppingmall.order.status.OrderStatus;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class OrderResponseDto {
-    private final Long orderid;
-    private final Long userid;
+    private final Long orderId;
+    /*private final User user;*/
     private final String address;
     private final int totalPrice;
     private final OrderStatus status;
 
-    @Builder
     public OrderResponseDto(Order order) {
-        this.orderid = order.getId();
-        this.userid = order.getUserid();
+        this.orderId = order.getId();
+        /*this.User = order.getUser();*/
         this.address = order.getAddress();
         this.totalPrice = order.getTotalPrice();
         this.status = order.getStatus();
